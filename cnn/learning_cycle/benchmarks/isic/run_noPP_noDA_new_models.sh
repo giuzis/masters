cefcf7c26a491d1a5b2da88021f7e82c1750a498
giuzis <giuliana.martins23@gmail.com>
2023-04-20 01:53:10 +0100

    update files

--- cnn/learning_cycle/benchmarks/isic/run_noPP_noDA_new_models.sh
+++ cnn/learning_cycle/benchmarks/isic/run_noPP_noDA_new_models.sh
@@ -5,31 +5,31 @@ declare -a modelArray=("resnext101_32x8d" "efficientnet_b0" "efficientnet_b1" "e
 declare -a optArray=("SGD" "Adam" "AdamW")
 declare -a batch_size=(4 8 16 32 64)
 
-python isic.py with _model_name=tf_efficientnet_b5 _lr_init=0.0001 _batch_size=8 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=tf_efficientnet_b6 _lr_init=0.0001 _batch_size=4 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=densenet121 _lr_init=0.0001 _batch_size=32 _optimizer=Adam _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=efficientnet_b0 _lr_init=0.0001 _batch_size=8 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=efficientnet_b1 _lr_init=0.0001 _batch_size=8 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=efficientnet_b2 _lr_init=0.0001 _batch_size=8 _optimizer=Adam _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=efficientnet_b3 _lr_init=0.0001 _batch_size=32 _optimizer=Adam _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=efficientnet_b4 _lr_init=0.0001 _batch_size=16 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=resnest101e _lr_init=0.0001 _batch_size=8 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=seresnext101_32x8d _lr_init=0.0001 _batch_size=16 _optimizer=Adam _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=vgg19 _lr_init=0.0001 _batch_size=8 _optimizer=SGD _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=resnext101_32x8d _lr_init=0.0001 _batch_size=8 _optimizer=SGD _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=pnasnet5large _lr_init=0.0001 _batch_size=8 _optimizer=Adam _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder
+python isic.py with _model_name=tf_efficientnet_b5 _lr_init=0.0001 _batch_size=8 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder _data_augmentation=2 
+python isic.py with _model_name=tf_efficientnet_b6 _lr_init=0.0001 _batch_size=4 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=densenet121 _lr_init=0.0001 _batch_size=32 _optimizer=Adam _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=efficientnet_b0 _lr_init=0.0001 _batch_size=8 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=efficientnet_b1 _lr_init=0.0001 _batch_size=8 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=efficientnet_b2 _lr_init=0.0001 _batch_size=8 _optimizer=Adam _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=efficientnet_b3 _lr_init=0.0001 _batch_size=32 _optimizer=Adam _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=efficientnet_b4 _lr_init=0.0001 _batch_size=16 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=resnest101e _lr_init=0.0001 _batch_size=8 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=seresnext101_32x8d _lr_init=0.0001 _batch_size=16 _optimizer=Adam _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=vgg19 _lr_init=0.0001 _batch_size=8 _optimizer=SGD _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=resnext101_32x8d _lr_init=0.0001 _batch_size=8 _optimizer=SGD _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=pnasnet5large _lr_init=0.0001 _batch_size=8 _optimizer=Adam _epochs=150 _early_stop=100 _PP_hair_removal=dull_razor _PP_color_constancy=shades_of_gray _PP_crop_mode=cropped_images_folder _data_augmentation=2
 
-python isic.py with _model_name=tf_efficientnet_b5 _lr_init=0.0001 _batch_size=8 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=tf_efficientnet_b6 _lr_init=0.0001 _batch_size=4 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=densenet121 _lr_init=0.0001 _batch_size=32 _optimizer=Adam _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=efficientnet_b0 _lr_init=0.0001 _batch_size=8 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=efficientnet_b1 _lr_init=0.0001 _batch_size=8 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=efficientnet_b2 _lr_init=0.0001 _batch_size=8 _optimizer=Adam _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=efficientnet_b3 _lr_init=0.0001 _batch_size=32 _optimizer=Adam _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=efficientnet_b4 _lr_init=0.0001 _batch_size=16 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=resnest101e _lr_init=0.0001 _batch_size=8 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=seresnext101_32x8d _lr_init=0.0001 _batch_size=16 _optimizer=Adam _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=vgg19 _lr_init=0.0001 _batch_size=8 _optimizer=SGD _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=resnext101_32x8d _lr_init=0.0001 _batch_size=8 _optimizer=SGD _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder
-python isic.py with _model_name=pnasnet5large _lr_init=0.0001 _batch_size=8 _optimizer=Adam _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder
+python isic.py with _model_name=tf_efficientnet_b5 _lr_init=0.0001 _batch_size=8 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=tf_efficientnet_b6 _lr_init=0.0001 _batch_size=4 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=densenet121 _lr_init=0.0001 _batch_size=32 _optimizer=Adam _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=efficientnet_b0 _lr_init=0.0001 _batch_size=8 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=efficientnet_b1 _lr_init=0.0001 _batch_size=8 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=efficientnet_b2 _lr_init=0.0001 _batch_size=8 _optimizer=Adam _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=efficientnet_b3 _lr_init=0.0001 _batch_size=32 _optimizer=Adam _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=efficientnet_b4 _lr_init=0.0001 _batch_size=16 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=resnest101e _lr_init=0.0001 _batch_size=8 _optimizer=AdamW _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=seresnext101_32x8d _lr_init=0.0001 _batch_size=16 _optimizer=Adam _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=vgg19 _lr_init=0.0001 _batch_size=8 _optimizer=SGD _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=resnext101_32x8d _lr_init=0.0001 _batch_size=8 _optimizer=SGD _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder _data_augmentation=2
+python isic.py with _model_name=pnasnet5large _lr_init=0.0001 _batch_size=8 _optimizer=Adam _epochs=150 _early_stop=100 _PP_crop_mode=cropped_images_folder _data_augmentation=2
 
